f614d3b78aa3c41b4d16286d6a43bfecfe74e4ea
Kelly <20522051@gm.uit.edu.vn>
2022-11-07 00:06:05 +0700

    add schema in models

--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const taskSchema = new mongoose.Schema(
-	
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      require: false,
+    },
+    status: {
+      type: String,
+      require: true,
+      enum: ["pending", "working", "review", "done", "archive"],
+    },
+    referenceTo:{type: mongoose.SchemaTypes.ObjectId, ref: "User"}
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const Task = mongoose.model('Task', taskSchema);
+const Task = mongoose.model("Task", taskSchema);
 
 module.exports = Task;
--- models/User.js
+++ models/User.js
@@ -1,8 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
-	
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    role: {
+      type: String,
+      require: true,
+      enum: ["manager", "employee"],
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
